5b67de107049b45579984066ff3c07007e73b089
Yan Ming <Ming@Yans-MacBook-Pro.local>
2019-02-10 21:09:29 +0800

    first commit of 05_functions

--- labs/05_functions/05_01_tasks.py
+++ labs/05_functions/05_01_tasks.py
@@ -14,3 +14,41 @@ Write a script that completes the following tasks.
 # calls a function that determines whether the number is divisible by 4 or 7 exclusively
 
 # print the results
+
+user_input = int(input("Please enter a number from 1 to 1,000,000,000: "))
+
+
+def div_4n7(num):
+    # function that checks if a number is divisible by both 4 AND 7.
+    if num % 4 == 0 and num % 7 == 0:
+        return "Yes, it's divisible by both 4 and 7."
+    else:
+        return "Sorry, it's not divisible by both 4 and 7"
+
+
+def div_4o7(num):
+    # function that checks if a number is divisible by 4 or 7.
+    if num % 4 == 0 or num % 7 == 0:
+        return "Yes, it's divisible by 4 or 7."
+    else:
+        return "Sorry, it's not divisible by 4 or 7"
+
+
+def div_4_7(num):
+    # function that shows if a number is divisible by either 4 or 7, or by both.
+    if num % 4 == 0:
+        if num % 7 == 0:
+            return "It's divisible by 4 and 7."
+        else:
+            return "It's only divisible by 4, not by 7."
+
+    else:
+        if num % 7 == 0:
+            return "It's only divisible by 7, not by 4."
+        else:
+            return "Sorry, it's not divisible by 4 nor 7."
+
+
+print(div_4n7(user_input))
+print(div_4o7(user_input))
+print(div_4_7(user_input))
--- labs/05_functions/05_02_rps.py
+++ labs/05_functions/05_02_rps.py
@@ -2,36 +2,57 @@
 Code a game of rock paper scissors.
 
 '''
+import random
 
 
 # function to get hand based on number
 # the function should take in a number and return the string representation of the hand
 def get_hand(hand):
     # 0 = scissor, 1 = rock, 2 = paper
-
     # for example if the variable hand is 0, it should return the string "scissor"
-    pass
+    if hand == 0:
+        return "scissor"
+    elif hand == 1:
+        return "rock"
+    elif hand == 2:
+        return "paper"
+    else:
+        return "Wrong input!"
+
 
 # function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
 def determine_winner(computer, player):
-    pass
-
+    if computer == player:
+        return "You tied"
+    elif (computer == "scissor" and player == "paper") or \
+            (computer == "rock" and player == "scissor") or \
+            (computer == "paper" and player == "rock"):
+        return "You lost :("
+    else:
+        return "You won!"
 
 
 '''
 Start here
 '''
-
+wrong_input = True
+player_hand = 0 # default value for player hand
 # take in a number 0-2 from the user that represents their hand
-
+while wrong_input:
+    player_hand = int(input("Please enter an integer to show your hand (0, 1, 2): "))
+    if player_hand in (0, 1, 2):
+        wrong_input = False
+    else:
+        print("Your number is out of range!")
 # generate a random number 0-2 to use for the computer's hand
-
+pc_hand = random.randint(0, 2)
 # call the function get_hand to get the string representation of the user's hand
-
+player = get_hand(player_hand)
 # call the function get_hand to get the string representation of the computer's hand
-
+computer = get_hand(pc_hand)
 # call the function determine_winner to figure out the winner
-
+result = determine_winner(computer, player)
 # print out the player hand and computer hand
-
+print(f"The player's hand is {player} and the computer's hand is {computer}.")
 # print out the winner
+print(result)
