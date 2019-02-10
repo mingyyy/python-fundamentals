27baa95b2c847dd187cfdb50f49205cf1de67733
Yan Ming <Ming@Yans-MacBook-Pro.local>
2019-02-10 15:05:17 +0800

    second commit of 04_conditionals_loop

--- labs/04_conditionals_loops/03_04_prime.py
+++ labs/04_conditionals_loops/03_04_prime.py
@@ -2,10 +2,13 @@
 Print out every prime number between 1 and 100.
 
 '''
+sum = 0
 for i in range(1, 101):
     n = 0
     for j in range(2, i):
-        if i % j == 0:
+        if i % j == 0: # prime number can only be divided by 1 and itself.
             n += 1
     if n == 0:
         print(f"{i} is a prime number")
+        sum += 1
+print(f"In total, there are {sum} prime numbers between 1 and 100. ")
